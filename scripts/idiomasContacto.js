9d22dffba95b221fdf67b60698617893e08fff5e
SpikeSoap <alazgorjs@gmail.com>
2023-10-11 12:59:57 +0200

    Traducción del apartado de contacto

---
+++ scripts/idiomasContacto.js
@@ -0,0 +1,64 @@
+const mensaje = {
+    title: [
+        "Want to talk?",
+        "Hitz egin nahi?"
+    ],
+    subTitle: [
+        "Send us a message",
+        "Bidaliguzu mezu bat"
+    ],
+    inputName: [
+        "Name",
+        "Izena"
+    ],
+    inputSur: [
+        "Surnames",
+        "Abizenak"
+    ],
+    inputTel: [
+        "Phone number",
+        "Tel. Zebakia"
+    ],
+    inputEmail: [
+        "Email",
+        "Posta elek."
+    ],
+    selectTitle: [
+        "What service are you interested in?",
+        "Zein zerbitzuan interesa duzu?"
+    ],
+    selecOp1: [
+        "Air-conditioning",
+        "Aire girotua"
+    ],
+    selecOp2: [
+        "Plumbing",
+        "Iturgintza"
+    ],
+    selectOp3: [
+        "Heating and gas",
+        "Berogailua eta gasa"
+    ],
+    selecOp4: [
+        "Alterarions",
+        "Berrriztatze"
+    ],
+    selecOp5: [
+        "Assembly",
+        "Muntaia"
+    ],
+    selecOp6:[
+        "Maintenance",
+        "Mantentze"
+    ],
+    textArea:[
+        "Tell us",
+        "Kontaiguzu"
+    ]
+};
+
+const detalles={
+title:[
+    "Contact details",
+    "Harremanetarako datuak",
+]};
